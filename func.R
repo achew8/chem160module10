fdd426ab09c009ee3fca634a319d8c9890fcb47e
achew8 <70230739+achew8@users.noreply.github.com>
2020-10-14 17:06:28 -0700

    Update func.R

--- func.R
+++ func.R
@@ -5,8 +5,4 @@ opt <- optimize(func,c(0.,5.))
 cat(opt$minimum,opt$objective)
 plot(func,0,5) 
 abline(h=opt$objective,col=2)
-abline(v=opt$minimum,col=2)  
-  
- 
- 
- 
+abline(v=opt$minimum,col=2) 
